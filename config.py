33243c561ad6a5542d17b27746a29acde5987597
louisld <louisg@bloomenetwork.fr>
2025-10-26 13:08:49 +0100

    Fix database connection

--- config.py
+++ config.py
@@ -5,6 +5,13 @@ class BaseConfig():
     STATIC_DIR = "static/assets"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS = os.environ.get("SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS", True)
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True,             
+        'pool_recycle': 1800,       
+        'pool_size': 10,            
+        'max_overflow': 20  
+    }
+
 
 class DevelopmentConfig(BaseConfig):
     DEBUG=True
